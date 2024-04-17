698ee7f4fc9f2230cc32ba3c314dcc72de62f2a4
WalterCorsini <corsini.walter@gmail.com>
2024-04-17 15:07:28 +0200

    add dynamic message and img with user input

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 
+// const {createApp} = Vue;
+
+// createApp({
+//     data() {
+//         return{
+//             variabile: "ciao",
+//             img: "./img/po.jpg",
+//         }
+//     },
+
+// }).mount("#app");
+
+
+//  take createApp function with "detructuring"
 const {createApp} = Vue;
 
+// call function createApp
 createApp({
-    data() {
-        return{
-            variabile: "ciao",
-            img: "./img/po.jpg",
+    // funnction return
+    data() {    
+        // container variable
+        return{             
+            userName: "",
+            inputUser:"",
+            img: "",
         }
+        // container variable
     },
 
-}).mount("#app");
+    // function container
+    methods: {
+        userHello: function(){
+            this.userName = "ciao " + this.inputUser;
+            this.img = "./img/po.jpg"
+            this.inputUser = "";
+        }
+    }
+    // function container
+}).mount("#app");// link to container DOM
